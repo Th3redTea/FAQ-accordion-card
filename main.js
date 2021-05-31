925e2fc1875561d40712b631e7d1a4f49d9d13d3
Amine Amhoume <amhoume@gmail.com>
2021-05-31 12:18:53 +0100

    completed the text section, needs some tweaks on the illustrations

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-const answers = document.querySelectorAll('#answer')
-const arrow = document.getElementById('arrow')
 
 
-function showAnswer() {
-    console.log('clicked')
-
-    answers.forEach((answer) => {
-        let style =  window.getComputedStyle(answer)
-
-    if (style.visibility === "hidden" ){
-        answer.style.visibility = 'visible'
-        answer.style.height = '60px'
-        arrow.style.transform = 'rotate(180deg)'
-        arrow.style.transition = 'transform 0.2s ease'
-    } else if(style.visibility === "visible"){
-        answer.style.visibility = 'hidden'
-        answer.style.height = "0px"
-        arrow.style.transform = 'rotate(0deg)'
-    }
-    })
-}
-
-arrow.addEventListener('click', showAnswer)
 
